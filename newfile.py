7ac12cc5ee054f3146cf931a824eb5489bb1b1fb
Imaginarminer <theimaginaryminermc@gmail.com>
2024-10-05 22:09:24 +0800

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,9 @@
+
+animal = input("Enter a Animal: ")
+vehicle = input("Enter a Vehicle: ")
+city = input("Enter a Name of a City: ")
+
+print("This " + animal + " is slower,")
+print("But this " + vehicle + " is faster,")
+print("and city of " + city + " beautiful,")
+print("But you are the most beautiful in my eyes🤍.")
